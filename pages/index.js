f34979c8aac300114970994e88f97436ab2c54e4
Fábio Novais <fabio.novais@gmail.com>
2025-07-07 22:06:34 -0300

    Adicionado bg escuro

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ function Home() {
 
             }}>
                 <h1 style={{'color': 'red', 'fontSize': '80px'}}>&#10084;</h1>
-                <p style={{'color': 'red', 'text-align': 'center', 'fontSize': '26px'}}>Te amo!</p>
+                <p style={{'text-align': 'center', 'fontSize': '26px'}}>Te amo!</p>
             </div>
         </>
     )
