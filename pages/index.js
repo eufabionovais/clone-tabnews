b7805ed78e86a801abdca113d2c53d8851199a0b
Fábio Novais <fabio.novais@gmail.com>
2025-07-07 21:41:17 -0300

    Adiciona mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 function Home() {
-    return <h1>Teste Next</h1>
+    return (
+        <>
+            <div style={{
+                'display': 'grid',
+                'height': '100%'
+
+            }}>
+                <h1 style={{'color': 'red', 'fontSize': '50px'}}>&#10084;</h1>
+                <p style={{'color': 'red'}}>Teste</p>
+            </div>
+        </>
+    )
 }
 
 export default Home;
