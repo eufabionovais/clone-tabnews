db2f27fecf9c5762371965fef1d171cefa87c8b8
Fábio Novais <fabio.novais@gmail.com>
2025-08-12 18:54:14 -0300

    Adiciona scripts

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
 function Home() {
-    return (
-        <>
-            <div style={{
-                'display': 'grid',
-                'height': '100%'
-
-            }}>
-                <h1 style={{'color': 'red', 'fontSize': '80px'}}>&#10084;</h1>
-                <p style={{'text-align': 'center', 'fontSize': '26px'}}>Te amo!</p>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div
+        style={{
+          display: "grid",
+          height: "100%",
+        }}
+      >
+        <h1 style={{ color: "red", fontSize: "80px" }}>&#10084;</h1>
+        <p style={{ "text-align": "center", fontSize: "26px" }}>Te amo!</p>
+      </div>
+    </>
+  );
 }
 
 export default Home;
